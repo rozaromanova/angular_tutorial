00073abfe17f8c8da2b0e47a2c88a7e2da402ef1
rozaromanova <roza.romanova@gmail.com>
2018-10-20 16:32:16 +0200

    storage service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,17 @@
 import { Component } from '@angular/core';
-import {TodoItem} from './interfaces/todo-item';
+
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>
-      Very warm welcome to {{ title }}!
+    <h1 class="app-title">
+      {{ title }}
     </h1>
-
-    <todo-input-button-unit (submit)="addItem($event)"></todo-input-button-unit>
-    <ul>
-      <li *ngFor="let item of todoList">
-        <todo-todo-item [item]="item"></todo-todo-item>
-      </li>
-    </ul>
+    <todo-list-manager></todo-list-manager>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
-  todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
-  addItem(title: string) {
-    this.todoList.push({ title });
-  }
+  title = 'Too much to do';
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { ListManagerComponent } from './list-manager/list-manager.component';
+import {TodoListService} from './services/todo-list.service';
+import {StorageService} from './services/storage.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputButtonUnitComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    ListManagerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [TodoListService,
+              StorageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
