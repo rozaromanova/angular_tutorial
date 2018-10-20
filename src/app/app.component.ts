544e27bc627d0bb7b2510c71d867f4ab45632d47
rozaromanova <roza.romanova@gmail.com>
2018-10-20 15:03:08 +0200

    adding input button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,23 @@ import { Component } from '@angular/core';
     </h1>
 
     <todo-input-button-unit></todo-input-button-unit>
+    <ul>
+      <li *ngFor="let item of todoList">
+        <todo-todo-item [item]="item"></todo-todo-item>
+      </li>
+    </ul>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo-list';
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
 }
 
